610ec2b329b87ee4a23edc01acab8b6981922834
Furkan Umut Ç. <155393351+FCUmut@users.noreply.github.com>
2024-08-27 23:48:50 +0300

    commit
    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
+import reactImg from "./assets/react-core-concepts.png";
+// import componentsImg from "./assets/components.png";
+import { CORE_CONCEPTS } from "./data.js";
+// since it is not default export but name export then you put curly braces to the imported value name
+
 const reactDescriptions = ["Fundemental", "Crucial", "Core"];
 
 function genRandomInt(max) {
@@ -8,7 +13,8 @@ function Header() {
   const description = reactDescriptions[genRandomInt(2)];
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      {/* <img src="src/assets/react-core-concepts.png" alt="Stylized atom" /> */}
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {description} React concepts you will need for almost any app you are
@@ -18,12 +24,50 @@ function Header() {
   );
 }
 
+function CoreConcept({ image, title, description }) {
+  // function CoreConcept(props) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header></Header>
       {/*<Header/> or this way*/}
       <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {/* <CoreConcept
+              title="Components"
+              description="The core UI building block."
+              img={componentsImg}
+            ></CoreConcept> */}
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+              image={CORE_CONCEPTS[1].image}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[2].title}
+              description={CORE_CONCEPTS[2].description}
+              image={CORE_CONCEPTS[2].image}
+            />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {/* shorther version */}
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
