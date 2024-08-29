820eb8cffab323c31a34b26bb05dbf710ed6abfd
Furkan Umut Ç. <155393351+FCUmut@users.noreply.github.com>
2024-08-29 22:48:20 +0300

    commit
    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,8 @@
-import reactImg from "./assets/react-core-concepts.png";
 // import componentsImg from "./assets/components.png";
 import { CORE_CONCEPTS } from "./data.js";
 // since it is not default export but name export then you put curly braces to the imported value name
-
-const reactDescriptions = ["Fundemental", "Crucial", "Core"];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-  return (
-    <header>
-      {/* <img src="src/assets/react-core-concepts.png" alt="Stylized atom" /> */}
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept({ image, title, description }) {
-  // function CoreConcept(props) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
+import Header from "./components/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
 
 function App() {
   return (
