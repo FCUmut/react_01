623a57cea46868abc9cf8858de4242aa40424415
Furkan Umut Ç. <155393351+FCUmut@users.noreply.github.com>
2024-10-05 23:57:02 +0300

    commit
    commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,10 @@ import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
+import { EXAMPLES } from "./data.js";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("Please click a button");
+  const [selectedTopic, setSelectedTopic] = useState("components");
   // must called in top level component function
 
   // let tabContent = "Plese click a button";
@@ -65,7 +66,14 @@ function App() {
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
           {/* {tabContent} */}
-          {selectedTopic}
+          {/* {selectedTopic} */}
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
         <h2>Time to get started!</h2>
       </main>
